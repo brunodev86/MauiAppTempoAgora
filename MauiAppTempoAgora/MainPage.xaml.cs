4b53c7f52711d289a67e708300a4dcbefdc0ca4f
bruno rosendo alves <brunora86@gmail.com>
2025-04-06 20:05:40 -0300

    Mensagem específica quando o nome da cidade não for encontrado. Alerta quando o usuário estiver sem conexão com a internet.

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -1,12 +1,11 @@
 ﻿using MauiAppTempoAgora.Models;
 using MauiAppTempoAgora.Services;
+using System.Net;
 
 namespace MauiAppTempoAgora
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
@@ -18,34 +17,51 @@ namespace MauiAppTempoAgora
             {
                 if (!string.IsNullOrEmpty(txt_cidade.Text))
                 {
-                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
-                    if (t != null) 
+                    HttpResponseMessage response = await DataService.GetHttpResponse(txt_cidade.Text);
+                    if (response.IsSuccessStatusCode)
                     {
-                        string dados_previsao = "";
-                        dados_previsao = $"Latitude:{t.lat} \n" +
-                                         $"Longitude:{t.lon} \n" +
-                                         $"Nascer do Sol:{t.sunrise} \n" +
-                                         $"Por do Sol:{t.sunset} \n" +
-                                         $"Temp Máxima:{t.temp_max} \n" +
-                                         $"Temp Miníma:{t.temp_min} \n";
+                        Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
+                        if (t != null)
+                        {
+                            string dados_previsao = $"Latitude: {t.lat} \n" +
+                                                    $"Longitude: {t.lon} \n" +
+                                                    $"Nascer do Sol: {t.sunrise} \n" +
+                                                    $"Por do Sol: {t.sunset} \n" +
+                                                    $"Temp Máxima: {t.temp_max}°C \n" +
+                                                    $"Temp Miníma: {t.temp_min}°C \n" +
+                                                    $"Descrição do Clima: {t.description} \n" +
+                                                    $"Velocidade do Vento: {t.speed} m/s \n" +
+                                                    $"Visibilidade: {t.visibility} m \n";
 
-                        lbl_res.Text = dados_previsao;
-
-                    }else 
+                            lbl_res.Text = dados_previsao;
+                        }
+                        else
+                        {
+                            lbl_res.Text = "Sem dados de previsão.";
+                        }
+                    }
+                    else if (response.StatusCode == HttpStatusCode.NotFound)
                     {
-                        lbl_res.Text = "Sem dados de Previsão";
+                        lbl_res.Text = "Cidade não encontrada.";
+                    }
+                    else
+                    {
+                        lbl_res.Text = "Erro ao obter dados de previsão.";
                     }
                 }
-                else 
+                else
                 {
                     lbl_res.Text = "Preencha a cidade.";
                 }
             }
-            catch (Exception ex) 
+            catch (HttpRequestException ex) when (ex.InnerException is System.Net.Sockets.SocketException)
+            {
+                await DisplayAlert("Erro", "Sem conexão com a internet.", "Ok");
+            }
+            catch (Exception ex)
             {
                 await DisplayAlert("Ops", ex.Message, "Ok");
             }
         }
     }
-
 }
--- MauiAppTempoAgora/Services/DataService.cs
+++ MauiAppTempoAgora/Services/DataService.cs
@@ -3,21 +3,31 @@ using Newtonsoft.Json.Linq;
 
 namespace MauiAppTempoAgora.Services
 {
-    public class DataService
+    public static class DataService
     {
-        public static async Task<Tempo?> GetPrevisao(string cidade) 
+        public static async Task<HttpResponseMessage> GetHttpResponse(string cidade)
         {
-            Tempo? t = null;
             string chave = "e3ea185b30a47e6eb59b2e9a4744c0d9";
             string url = $"https://api.openweathermap.org/data/2.5/weather?q={cidade}&units=metric&appid={chave}";
             using (HttpClient client = new HttpClient())
             {
+                return await client.GetAsync(url);
+            }
+        }
+
+        public static async Task<Tempo?> GetPrevisao(string cidade)
+        {
+            Tempo? t = null;
+            string chave = "e3ea185b30a47e6eb59b2e9a4744c0d9";
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={cidade}&units=metric&appid={chave}";
+
+            using (HttpClient client = new HttpClient())
+            {
                 HttpResponseMessage resp = await client.GetAsync(url);
 
-                if (resp.IsSuccessStatusCode) 
+                if (resp.IsSuccessStatusCode)
                 {
-                    string json =  await resp.Content.ReadAsStringAsync();
-
+                    string json = await resp.Content.ReadAsStringAsync();
                     var rascunho = JObject.Parse(json);
 
                     DateTime time = new();
@@ -36,7 +46,6 @@ namespace MauiAppTempoAgora.Services
                         visibility = (int)rascunho["visibility"],
                         sunrise = sunrise.ToString(),
                         sunset = sunset.ToString(),
-
                     };
                 }
             }
